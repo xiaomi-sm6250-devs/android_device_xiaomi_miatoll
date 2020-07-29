a1da94e2c2124a118e70a8f4bf9f2f02dbe19a1a
Erfan Abdi <erfangplus@gmail.com>
2020-07-29 18:13:00 +0430

    sm6250-common: Build Qti vibrator module
    Change-Id: If243497cf35745a16e778942a699ede6eccc3974

--- common.mk
+++ common.mk
@@ -469,8 +469,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator@1.2-service
 
 # Wifi
 PRODUCT_PACKAGES += \
