63e9fc9457e170e25c6cfa7c86d7fe87f00dad99
Volodymyr Zhdanov <wight554@gmail.com>
2020-08-10 17:33:14 +0300

    curtana: add NFC support for joyeuse
    Change-Id: Ia4f007aed2969b8f759644f3895c7cdbfa985354

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,6 +149,10 @@ LOC_HIDL_VERSION := 3.0
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
+ODM_MANIFEST_SKUS += \
+    nfc
+
+ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/manifests/manifest_nfc.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_atoll
